150632d92b12bccd33f679cacc57641b988ce491
carlosantencinas <64339850+carlosantencinas@users.noreply.github.com>
2026-09-25 09:57:29 -0400

    Agregar datos de contacto para cotizaciones

--- js/config.js
+++ js/config.js
@@ -8,6 +8,8 @@ const DEFAULT_CONFIG = {
     nombre: 'M.Sc. Ing. Carlos A. Antequera E.',
     rni: 'RNI: 28.106 Ingeniero Civil - Especialista Hidrólogo - Hidráulico',
     logo: LOGO_URL,
+    telefonoWhatsapp: '',
+    correo: '',
     defaultNota: 'Esta cotización se enfoca exclusivamente en servicios relacionados con la especialidad hidráulica e Hidrológica. No abarca el cálculo estructural de pasarelas, obras de captación de agua, ni ninguna otra actividad que pertenezca a especialidades diferentes tales como ambiental, geológica, topográfica, económica, agronómica, entre otras.',
     defaultEntregables: 'en formato digital:\n- Mapas parámetros morfométricos de la cuenca, pendientes, elevaciones, cobertura, precipitaciones, temperatura, estaciones y ubicación hidrográfica.\n- Informe Hidrológico, y modelo hidrológico.\n- Regulación del embalse.\n- Modelo numérico Vertedero Iber 3.3\n- Modelo numérico Aliviadero Iber 3.3\n- Planos Hidráulicos vertedor y aliviadero.\n- Modelo hidrodinámico Rotura de presa y zonas de riesgo Iber 3.3\n- Mapa de riesgo de inundación'
 };
